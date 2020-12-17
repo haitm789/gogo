51627e77d2f73028186306df1a40f6a180639516
jens789 <59217035+jens789@users.noreply.github.com>
2020-12-18 00:01:46 +0700

    .

--- src/app/api/infra/rest/router/v1/main.go
+++ src/app/api/infra/rest/router/v1/main.go
@@ -1,11 +1,7 @@
 package v1
 
 import (
-<<<<<<< HEAD:src/app/api/infra/rest/router/v1/main.go
 	"api/infra/rest/router/domain"
-=======
-	"api/infra/server/router/domain"
->>>>>>> b66ef7e55506fad641e3673c69db9b36f9ea0492:src/app/api/infra/rest/router/v1/v1.go
 )
 
 func Routes() domain.Group {
